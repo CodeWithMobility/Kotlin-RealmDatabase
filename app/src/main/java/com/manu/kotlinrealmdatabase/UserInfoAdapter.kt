f2a7c6770135351b4f9f9a3ca83ea8f0b52e5c2e
Manuaravind1989 <manuaravindpta@gmail.com>
2017-12-30 21:34:15 +0400

    initial commit-0

--- app/src/main/java/com/manu/kotlinrealmdatabase/UserInfoAdapter.kt
+++ app/src/main/java/com/manu/kotlinrealmdatabase/UserInfoAdapter.kt
@@ -8,9 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-
 import com.manu.kotlinrealmdatabase.model.UserInfo
 
 /**
